f7561b8b5c693ec7c21d27ee48a7d2b3aae80b97
José Deodanes <60304928+Jose-developer-start@users.noreply.github.com>
2022-09-11 21:17:18 -0600

    Update delCache.sh

--- delCache.sh
+++ delCache.sh
@@ -3,3 +3,7 @@ sync
 echo 3 > /proc/sys/vm/drop_caches
 echo 2> /proc/sys/vm/drop_caches
 ulimit -u 10000000
+#Aumentar el numero de procesos del sistema 
+#Docs: https://linoxide.com/limit-processes-user-level-linux/
+#* hard nproc 200000000
+#* hard   nofile   300000000
