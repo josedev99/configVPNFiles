7d2f11aaa313868afd0c23e2dd2b899a519813ab
José Deodanes <60304928+Jose-developer-start@users.noreply.github.com>
2022-09-19 11:22:33 -0600

    Update runproxy.sh

--- runproxy.sh
+++ runproxy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-status="false"
+status=false
 for port in $(lsof -i -P -n | grep python); do
         if [ $port = "*:80" ]; then
-                status="true"
+                status=true
         fi      
 done
 
-if [ $status = "true" ]; then
+if [ $status = true ]; then
         echo "Activado"
-	badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 300 --max-connections-for-client 10 --client-socket-sndbuf 0 --udp-mtu 9000 > /dev/null 2>&1 &
+	#badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 300 --max-connections-for-client 10 --client-socket-sndbuf 0 --udp-mtu 9000 > /dev/null 2>&1 &
+	screen -dmS screen /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 20
 else
         echo "Puerto inactivo"
 	python PDirect.py 80 > /dev/null 2>&1 &
-	badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 300 --max-connections-for-client 10 --client-socket-sndbuf 0 --udp-mtu 9000 > /dev/null 2>&1 &
+	screen -dmS screen /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 20
 fi
