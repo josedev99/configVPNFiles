89690ed58842a13dabdada181058e929c9f6dc21
José Deodanes <60304928+Jose-developer-start@users.noreply.github.com>
2022-09-19 21:29:37 -0600

    Create tcp_speed.sh

---
+++ tcp_speed.sh
@@ -0,0 +1,7 @@
+net.ipv4.tcp_window_scaling = 1
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 16384 16777216
+net.ipv4.tcp_low_latency = 1
+net.ipv4.tcp_slow_start_after_idle = 0
