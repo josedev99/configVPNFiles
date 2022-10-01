5fb08364caafcc3b530e303c6b43c8ecb4c921d7
José Deodanes <60304928+Jose-developer-start@users.noreply.github.com>
2022-09-30 22:44:27 -0600

    Update addV2rayClient.sh

--- addV2rayClient.sh
+++ addV2rayClient.sh
@@ -1,8 +1,15 @@
-valid=$(date '+%C%y-%m-%d' -d " +31 days")
+#!/bin/bash
+
+#Autor: hive-vpn.tk
+
+# params : $user => $1
+
+#valid=$(date '+%C%y-%m-%d' -d " +31 days")
+valid=$2
 ##CORREO
-MAILITO=$(cat /dev/urandom | tr -dc '[:alnum:]' | head -c 10)
+MAILITO="$1_$valid"
 ##ADDUSERV2RAY
-UUID=`uuidgen`
+UUID=`uuid`
 sed -i '13i\           \{' /etc/v2ray/config.json
 sed -i '14i\           \"alterId": 0,' /etc/v2ray/config.json
 sed -i '15i\           \"id": "'$UUID'",' /etc/v2ray/config.json
@@ -11,3 +18,8 @@ sed -i '17i\           \},' /etc/v2ray/config.json
 echo $UUID
 echo $UUID ':' $valid >> /root/v2ray.txt
 service v2ray restart
+
+#Guardamos los usuarios y la fecha de expiracion
+touch /etc/v2ray/users.txt
+
+echo "$MAILITO:$valid" >> /etc/v2ray/users.txt
