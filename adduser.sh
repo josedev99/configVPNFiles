2356521215264bb9601b0d4807cba2f0d90bfc09
José Deodanes <60304928+Jose-developer-start@users.noreply.github.com>
2022-10-18 20:40:39 -0600

    Update
    Update

--- adduser.sh
+++ adduser.sh
@@ -7,5 +7,5 @@ useradd -e $1 -p "$(mkpasswd --method=sha-512 $2)" -s /usr/sbin/nologin $3
 echo "$3 hard maxlogins $4" >> /etc/security/limits.conf
 #Replicar datos de usuario para balancear carga de servidor
 #apt install sshpass >> package
-sshpass -p 'password' ssh root@82.117.252.108 "useradd -e $1 -p '$(mkpasswd --method=sha-512 $2)' -s /usr/sbin/nologin $3"
-sshpass -p 'password' ssh root@82.117.252.108 "echo $3 hard maxlogins $4 >> /etc/security/limits.conf"
+#sshpass -p 'password' ssh root@82.117.252.108 "useradd -e $1 -p '$(mkpasswd --method=sha-512 $2)' -s /usr/sbin/nologin $3"
+#sshpass -p 'password' ssh root@82.117.252.108 "echo $3 hard maxlogins $4 >> /etc/security/limits.conf"
